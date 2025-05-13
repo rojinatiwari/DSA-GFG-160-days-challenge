9a14d80e1895d55e88391fe551c69e6cc5297d33
rojinatiwari <rojinatiwari17@gmail.com>
2025-05-13 22:07:45 +0545

    Maximum sub array

---
+++ Day10_Kadane's_Algorithm.cpp
@@ -0,0 +1,37 @@
+// Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+// Examples:
+
+// Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+// Output: 11
+// Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+// Input: arr[] = [-2, -4]
+// Output: -2
+// Explanation: The subarray {-2} has the largest sum -2.
+// Input: arr[] = [5, 4, 1, 7, 8]
+// Output: 25
+// Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+// Constraints:
+// 1 ≤ arr.size() ≤ 105
+// -109 ≤ arr[i] ≤ 104
+
+
+//solution
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        int sum =0, maxi=arr[0];
+        for(int i = 0 ; i<n; i++)
+    {
+        sum += arr[i];
+        maxi = max(sum,maxi);
+        if(sum<0)
+        {
+            sum =0;
+        }
+    }
+        return maxi;
+    }
+};
